539224e60267015481812455df4b2665070d344c
MONI-ZISAN <massengerbotmz@gmail.com>
2025-09-15 01:05:46 +0600

    Update islamic.js

--- Script/commands/islamic.js
+++ Script/commands/islamic.js
@@ -4,7 +4,7 @@ module.exports.config = {
  hasPermssion: 0,
  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
  description: "RANDOM islamic video",
- commandCategory: "Random video",
+ commandCategory: "admin",
  usages: "Statusvideo",
  cooldowns: 2,
  dependencies: {
